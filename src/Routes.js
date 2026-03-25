8e952fe05681eb97f070bb5c4c10c1961ab872a9
HaileA2 <hailea536@gmail.com>
2026-03-25 13:42:10 +0300

    this is the first commit

--- src/Routes.js
+++
@@ -1,8 +0,0 @@
-//main router setup goes here
-import { Router } from "express";
-const router=Router();
-import authRoutes from './modules/auth/auth.route.js';
-
-router.use('/auth',authRoutes);
-
-export default router;
